1b5854ba513b6fa9683a9892f15d85da1c47f494
Davis Tran <datadaev@gmail.com>
2023-06-27 21:53:59 -0700

    Added scoring system to game

--- javascript.js
+++ javascript.js
@@ -6,26 +6,45 @@ let getComputerChoice = () => {
 }
 
 let playRound = (playerSelection, computerSelection) => {
-    (playerSelection === computerSelection) ? console.log("You Tie!") :
-    (playerSelection === "rock" && computerSelection === "paper") ? console.log("You lose! Paper beats rock!") :
-    (playerSelection === "paper" && computerSelection === "rock") ? console.log("You win! Paper beats rock!") :
-    (playerSelection === "scissors" && computerSelection === "paper") ? console.log("You win! Scissors beats paper!") :
-    (playerSelection === "rock" && computerSelection === "scissors") ? console.log("You win! Scissors beats rock!") :
-    (playerSelection === "paper" && computerSelection === "scissors") ? console.log("You lose! Scissors beats paper!") :
-    (playerSelection === "scissors" && computerSelection === "rock") ? console.log("You lose! Rock beats scissors!") :
-    console.log("Please choose rock, paper, or scissors.");
+    let message = (playerSelection === computerSelection) ? "You Tie!" :
+    (playerSelection === "rock" && computerSelection === "paper") ? "You lose! Paper beats rock!" :
+    (playerSelection === "paper" && computerSelection === "rock") ? "You win! Paper beats rock!" :
+    (playerSelection === "scissors" && computerSelection === "paper") ? "You win! Scissors beats paper!" :
+    (playerSelection === "rock" && computerSelection === "scissors") ? "You win! Scissors beats rock!" :
+    (playerSelection === "paper" && computerSelection === "scissors") ? "You lose! Scissors beats paper!" :
+    (playerSelection === "scissors" && computerSelection === "rock") ? "You lose! Rock beats scissors!" :
+    "Please choose rock, paper, or scissors.";
 
+    return message
 }
 
-
-
 let game = () => {
+    let computerScore = 0;
+    let playerScore = 0;
+
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("Please choose rock, paper, or scissors").toLowerCase();
         console.log(playerSelection);
         const computerSelection = getComputerChoice().toLowerCase();
         console.log(computerSelection);
-        playRound(playerSelection, computerSelection);
+        const round = playRound(playerSelection, computerSelection);
+        console.log(round);
+        if (round.includes("lose")){
+            computerScore += 1;
+        } else if (round.includes("win")){
+            playerScore += 1;
+        } else {
+            playerScore += 0;
+            computerScore += 0;
+        }
+    }
+
+    if (playerScore > computerScore){
+        console.log("You won the match!")
+    } else if (computerScore > playerScore) {
+        console.log("You lost the match!")
+    } else {
+        console.log("It's a tie!")
     }
 }
 
